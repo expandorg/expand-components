912e9139de1f5e9ff6b3c9d4bdada3b1b858e59e
Slava Sobolev <sobolevslava@gmail.com>
2019-01-18 16:18:35 +0300

    comb lib 0.2.0

--- packages/modules/index.js
+++ packages/modules/index.js
@@ -1,5 +1,4 @@
 // @flow
-
 import {
   Form,
   formProps,
--- packages/validation/index.js
+++ packages/validation/index.js
@@ -1,6 +1,8 @@
 // @flow
+
 import validateForm from './src/validateForm';
 import validateField from './src/validateField';
 import rules from './src/rules';
+
 
 export { validateForm, validateField, rules };
