24973bddd36df7211995f4aeaa296fa87318108e
Slava Sobolev <sobolevslava@gmail.com>
2019-05-30 12:28:03 +0300

    new docz

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-flow', '@babel/preset-react'],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-throw-expressions',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-  ],
-};
