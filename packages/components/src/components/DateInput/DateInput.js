3362e0868769c12fb7b180acece0435908efedfd
Slava Sobolev <sobolevslava@gmail.com>
2019-07-25 15:03:03 +0300

    day picker customize

--- packages/components/src/components/DateInput/DateInput.js
+++ packages/components/src/components/DateInput/DateInput.js
@@ -10,7 +10,7 @@ import parse from 'date-fns/parse';
 
 import './DateInput.styl';
 
-const parseDate = (str, fmt, locale) => {
+const dateFnsparseDate = (str, fmt, locale) => {
   const parsed = parse(str, fmt, { locale });
   if (DateUtils.isDate(parsed)) {
     return parsed;
@@ -18,51 +18,48 @@ const parseDate = (str, fmt, locale) => {
   return undefined;
 };
 
-const formatDate = (date, fmt, locale) => format(date, fmt, { locale });
+const dateFnsformatDate = (date, fmt, locale) => format(date, fmt, { locale });
 
 export default class DateInput extends Component {
   static propTypes = {
-    value: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    placeholder: PropTypes.string,
+    inputProps: PropTypes.shape({}),
     theme: PropTypes.oneOf(['default', 'white']),
     error: PropTypes.bool,
-    dayPickerProps: PropTypes.shape({}),
-    onChange: PropTypes.func.isRequired,
+    format: PropTypes.string,
+    formatDate: PropTypes.func,
+    parseDate: PropTypes.func,
   };
 
   static defaultProps = {
-    value: undefined,
     theme: 'default',
-    placeholder: null,
     error: false,
-    dayPickerProps: undefined,
+    inputProps: {},
+    format: 'MM/DD/YYYY',
+    formatDate: dateFnsformatDate,
+    parseDate: dateFnsparseDate,
   };
 
   render() {
     const {
-      onChange,
-      name,
-      placeholder,
-      value,
       error,
       theme,
-      dayPickerProps,
+      formatDate,
+      parseDate,
+      inputProps,
+      ...rest
     } = this.props;
     return (
       <DayPickerInput
-        value={value}
         inputProps={{
+          ...inputProps,
           className: cn(`DayPickerInput-theme-${theme}`, {
             'DayPickerInput-error': error,
           }),
         }}
-        dayPickerProps={dayPickerProps}
-        placeholder={placeholder}
         formatDate={formatDate}
         format="MM/DD/YYYY"
         parseDate={parseDate}
-        onDayChange={date => onChange({ target: { name, value: date } })}
+        {...rest}
       />
     );
   }
