23c6d3d7ed59fc675c4e8df3517f641ef5ddbbc0
Slava Sobolev <sobolevslava@gmail.com>
2019-08-15 17:43:51 +0300

    input validation on

--- packages/validation/src/rules.js
+++ packages/validation/src/rules.js
@@ -9,6 +9,40 @@ export const rules = {
   isNumber: (value: any) =>
     typeof value !== 'undefined' && isNumeric(value.toString()),
   isRequiredArray: (value: any) => !!value && value.length > 0,
+  isMinCharacterCount: (value: any, min: number) => {
+    if (typeof value !== 'string') {
+      return false;
+    }
+    return value.length >= min;
+  },
+  isMaxCharacterCount: (value: any, max: number) => {
+    if (typeof value !== 'string') {
+      return false;
+    }
+    return value.length <= max;
+  },
+  isGreater: (value: any, b: number) => {
+    const v = +value;
+    return v > b;
+  },
+  isGreaterOrEqual: (value: any, a: number) => {
+    if (!rules.isNumber(value)) {
+      return false;
+    }
+    return +value >= a;
+  },
+  isLess: (value: any, a: number) => {
+    if (!rules.isNumber(value)) {
+      return false;
+    }
+    return +value < a;
+  },
+  isLessOrEqual: (value: any, a: number) => {
+    if (!rules.isNumber(value)) {
+      return false;
+    }
+    return +value <= a;
+  },
 };
 
 export default rules;
